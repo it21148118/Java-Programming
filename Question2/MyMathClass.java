d5da7e1d022edbf4b9ddc6804489c81b360ff630
Mehara Sahabandu <87423149+it21148118@users.noreply.github.com>
2022-07-04 20:20:27 +0530

    Update MyMathClass.java

--- Question2/MyMathClass.java
+++ Question2/MyMathClass.java
@@ -1,5 +1,28 @@
 package Question2;
 
-public class MyMathClass {
+import java.util.*;
+
+
+public class MyMathClass <T,V extends Number> {
+	
+	public void average(HashMap<T,V> hashMap) {
+		double tot=0;
+		
+		for(Map.Entry<T, V>m:hashMap.entrySet()) {
+			tot+=m.getValue().doubleValue();
+		}
+		
+		System.out.println("Average: "+tot/hashMap.size());
+	}
+	
+	public ArrayList<V> convertTo(HashMap<T,V> hashMap){
+		ArrayList<V> list=new ArrayList<>();
+		
+		for(Map.Entry<T, V>m: hashMap.entrySet()) {
+			list.add(m.getValue());
+		}
+		
+		return list;
+	}
 
 }
