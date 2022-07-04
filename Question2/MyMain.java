67b232ffc34c7e2324bebe444df39161483e6918
Mehara Sahabandu <87423149+it21148118@users.noreply.github.com>
2022-07-04 20:20:05 +0530

    Update MyMain.java

--- Question2/MyMain.java
+++ Question2/MyMain.java
@@ -1,9 +1,24 @@
 package Question2;
 
+import java.util.*;
+
+
 public class MyMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashMap<Integer,Double> map=new HashMap<>();
+		
+		MyMathClass<Integer,Double>mathClass=new MyMathClass<>();
+		
+		map.put(1, 10.0);
+		map.put(2, 20.0);
+		map.put(3, 30.0);
+		map.put(4, 40.0);
+		map.put(5, 50.0);
+		
+		mathClass.average(map);
+		System.out.println(mathClass.convertTo(map));
 
 	}
 
